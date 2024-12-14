123b5b3846a884dcad668b7e41152d705dbe97bc
El3ci0Sz <137710642+El3ci0Sz@users.noreply.github.com>
2024-12-13 21:35:02 -0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from cgra.cgra import CGRA
 from utils.mapping_generator import Mapping_generator
+from utils.graph_transformer import Graph_Transformer
+from cgra.cgra import CGRA
 
 def main():
+    #Testes
     cgra_dim = (4, 4)
     arch_name = "Teste5"
     cgra = CGRA(cgra_dim, arch_name)
@@ -17,8 +20,7 @@ def main():
     mapping_generator = Mapping_generator(len(graph_edges), graph_edges, II=1, alpha=alpha, cgra=cgra.__class__, cgra_dim=cgra_dim, arch_name=arch_name)
 
     mapped_nodes = mapping_generator.mapping(graph_edges, cgra, alpha)
+    print(cgra.edges)
     print(mapped_nodes)
-
-
 if __name__ == '__main__':
     main()
