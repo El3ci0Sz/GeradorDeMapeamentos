a6d918f74ae90586dafee36e7c6e8db6ff8bf251
El3ci0Sz <137710642+El3ci0Sz@users.noreply.github.com>
2025-01-11 00:27:27 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,39 @@
-from cgra.cgra import CGRA
-from utils.mapping_generator import Mapping_generator
-from utils.graph_transformer import Graph_Transformer
-from cgra.cgra import CGRA
+from cgra import CGRA
+from Mapping import Mapping
+from mapping_generator import Mapping_generator
+import traceback
+import networkx as nx
+import matplotlib.pyplot as plt
 
-def main():
-    #Testes
+def functional_test():
+
     cgra_dim = (4, 4)
-    arch_name = "Teste5"
+    arch_name = "TestArchitecture"
     cgra = CGRA(cgra_dim, arch_name)
 
-    graph_edges = [
-        (0, 1),
-        (1, 2),
-        (2, 3)
-    ]
+    dfg_tam = 10
+    II = 2
+    alpha = 0.6
+    alpha2 = 0.3
 
-    alpha = 2
+    mapping_generator = Mapping_generator(dfg_tam, II, alpha, alpha2, cgra)
 
-    mapping_generator = Mapping_generator(len(graph_edges), graph_edges, II=1, alpha=alpha, cgra=cgra.__class__, cgra_dim=cgra_dim, arch_name=arch_name)
+    print("=== Teste: map_dfg_to_cgra ===")
+    mapping = mapping_generator.mapp()
+    print("\n\nArestas")
+    print(mapping.dfg_edges)
+    print("\n\nResultados do Mapeamento:")
+    print("- Placement (Posições dos Nós no CGRA):")
+    for node, position in mapping.placement.items():
+        print(f"  Nó {node}: {position}")
+    
+    print("\n- DFG Edges (Conexões no DFG):")
+    for node, neighbors in mapping.dfg_edges.items():
+        print(f"  Nó {node}: {neighbors}")
 
-    mapped_nodes = mapping_generator.mapping(graph_edges, cgra, alpha)
-    print(cgra.edges)
-    print(mapped_nodes)
-if __name__ == '__main__':
-    main()
+    print("\n- Routing (Roteamento das Arestas):")
+    for route in mapping.routing:
+        print(f"  {route}")
+
+if __name__ == "__main__":
+    functional_test()
