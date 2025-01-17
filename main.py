a9ae919f2f0d645cccddbdaab0ecd1a280822696
El3ci0Sz <137710642+El3ci0Sz@users.noreply.github.com>
2025-01-16 21:08:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from src.cgra.cgra import CGRA
 from src.utils.Mapping import Mapping
 from src.utils.mapping_generator import Mapping_generator
-def functional_test():
+from src.utils.Graph_Visualizer import Graph_Visualizer
 
+def functional_test():
     cgra_dim = (4, 4)
     arch_name = "TestArchitecture"
     cgra = CGRA(cgra_dim, arch_name)
 
     dfg_tam = 10
-    II = 2
+    II = 3
     alpha = 0.6
     alpha2 = 0.3
 
@@ -30,6 +31,12 @@ def functional_test():
     print("\n- Routing (Roteamento das Arestas):")
     for route in mapping.routing:
         print(f"  {route}")
+
+    Graph_Visualizer.export_to_dot(mapping, "example_graph.dot")
+    Graph_Visualizer.generate_image_from_dot("example_graph.dot", "example_graph.png")
+    Graph_Visualizer.plot_cgra(mapping, cgra_dim, routing=False, output_file="placement_only.png")
+    Graph_Visualizer.plot_cgra(mapping, cgra_dim, routing=True, output_file="placement_and_routing.png")
+
 
 if __name__ == "__main__":
     functional_test()
