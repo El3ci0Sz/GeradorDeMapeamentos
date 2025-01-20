d11224bb3428596e2256d605f2661c96b88cd749
El3ci0Sz <137710642+El3ci0Sz@users.noreply.github.com>
2025-01-19 21:12:50 -0300

    Fix: O atributo dfg_vertices
    Agora é uma lista com todos os vertices, ao invez de ser apenas o numero de vertices.

--- src/utils/Mapping.py
+++ src/utils/Mapping.py
@@ -1,6 +1,6 @@
 class Mapping:
-  def __init__(self,vertices):
-    self.routing = []
+  def __init__(self,num_vertices):
+    self.routing = {}
     self.placement: dict = {}
     self.dfg_edges: dict = {}
-    self.dfg_vertices = vertices
+    self.dfg_vertices = list(range(num_vertices)) 
